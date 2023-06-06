a1f9c6c15692cdef61081ab0303be10301676d10
SMK TI BAZMA 10 <bintang684@smk.belajar.id>
2023-06-06 16:39:34 +0700

    project kampus

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Livewire\Pages\CategoriesComponent;
+use App\Http\Livewire\Pages\HomeComponent;
+use App\Http\Livewire\Pages\BooksComponent;
+use App\Http\Livewire\Pages\Categories\CreateComponent;
+use App\Http\Livewire\Pages\Categories\UpdateComponent;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get("/", HomeComponent::class)->name("dashboard");
+Route::get("/categories", CategoriesComponent::class)->name("categories");
+Route::get("/books", BooksComponent::class)->name("books");
+Route::get("/categories/create", CreateComponent::class)->name("categories.create");
+Route::get("/categories/{categoriesId}/update", UpdateComponent::class)->name("categories.update");
